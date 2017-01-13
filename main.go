02a5a0e78cf71e7ff5a76f23b9c5b6643bc50937
songyun <songyun@youku.com>
2017-01-13 18:53:42 +0800

    添加自定义错误页面路由及显示

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/mingzhehao/scloud/controllers"
 	"github.com/mingzhehao/scloud/g"
 	_ "github.com/mingzhehao/scloud/routers"
 )
 
 func main() {
 	g.InitEnv()
+	beego.ErrorController(&controllers.ErrorController{})
 	beego.Run()
 }
